30849b4924cd24c1df77d8c0792e38799c0dd5c3
DESKTOP-96KRNN2\motta <benmotta@gmail.com>
2021-02-11 16:41:24 -0500

    Ajuste de modulo de calendario

---
+++ application/modules/calendario/controllers/Calendario.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Calendario extends CI_Controller {
+	
+    public function __construct() {
+        parent::__construct();
+        $this->load->model("calendario_model");
+		$this->load->helper('form');
+    }
+
+	/**
+	 * Calendario
+     * @since 6/1/2021
+     * @author BMOTTAG
+	 */
+	public function index()
+	{
+			$data["view"] = 'calendar';
+			$this->load->view("layout_calendar", $data);
+	}
+	
+
+
+	
+	
+}
