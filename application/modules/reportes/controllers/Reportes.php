c5b0062b62f1725940fee248a6976e11ad7c9065
DESKTOP-96KRNN2\motta <benmotta@gmail.com>
2021-02-18 08:30:51 -0500

    Reporte: Ajuste de estilos

--- application/modules/reportes/controllers/Reportes.php
+++ application/modules/reportes/controllers/Reportes.php
@@ -39,7 +39,7 @@ class Reportes extends CI_Controller {
 			$pdf->SetSubject('TCPDF Tutorial');
 
 			// set default header data
-			$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, 'RESERVAS', 'ID #: ' . $idHorario . "\nFecha: " . $fecha, array(0,64,255), array(0,64,128));			
+			$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, 'RESERVAS', 'Fecha: ' . $fecha . "\nHorario: " . $horario, array(94,164,49), array(147,204,110));			
 
 			// set header and footer fonts
 			$pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
