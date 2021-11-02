687b8e76328e9804c3d4ce3a8b000958ffca54e5
Benjamin <benmotta@gmail.com>
2021-11-02 11:54:41 -0500

    Update Settings_model.php
    Se ajusta error de eliminacion de la creacion de horarios

--- application/modules/settings/models/Settings_model.php
+++ application/modules/settings/models/Settings_model.php
@@ -107,6 +107,80 @@
 					return false;
 				}
 	    }
+
+		/**
+		 * Add/Edit Horario
+		 * @since 16/2/2021
+		 */
+		public function saveHorarios() 
+		{
+				$intervalo = $this->input->post('intervalo');
+				$fechaInicio = $this->input->post('start_date');
+				$fechaFin = $this->input->post('finish_date');
+				$horaInicio = $this->input->post('start_hour');
+				$horaFin = $this->input->post('finish_hour');
+
+				$date1 = new DateTime($fechaInicio);
+				$date2 = new DateTime($fechaFin);
+				$diff = $date1->diff($date2);
+				$numeroDias = $diff->days + 1;
+
+				switch ($intervalo) {
+					case 1:
+						//cada 15 min
+						$incremento = '+15 minute';
+						$numeroHorariosDia = ($horaFin - $horaInicio)*4;
+						break;
+					case 2:
+						//cada 30 min
+						$incremento = '+30 minute';
+						$numeroHorariosDia = ($horaFin - $horaInicio)*2;
+						break;
+					case 3:
+						//cada 60 min
+						$incremento = '+60 minute';
+						$numeroHorariosDia = ($horaFin - $horaInicio);
+						break;
+				}
+
+				$numeroHorariosDia++;
+
+				$fechaInicial = $fechaInicio . ' ' . $horaInicio . ':00:00';
+				
+				for ($i = 0; $i < $numeroDias; $i++) 
+				{
+					$date = new DateTime($fechaInicial);
+					$date->modify('+' . $i . ' day');
+					$horarioInicio = $date->format('Y-m-d H:i:s');
+
+					for ($y = 0; $y < $numeroHorariosDia; $y++)
+					{
+						$date = new DateTime($horarioInicio);
+						$date->modify($incremento);
+						$horaFinal = $date->format('Y-m-d H:i:s');
+
+						$data = array(
+							'hora_inicial' => $horarioInicio,
+							'hora_final' => $horaFinal,
+							'numero_cupos' => $this->input->post('numeroCupos'),
+							'numero_cupos_restantes' => $this->input->post('numeroCupos'),
+							'estado' => 1,
+							'disponible' => 1
+						);
+						$query = $this->db->insert('horarios', $data);
+
+						$horarioInicio = $horaFinal;
+					}
+
+				}
+			
+
+				if ($query) {
+					return true;
+				} else {
+					return false;
+				}
+		}	    
 		
 		/**icionar cupos/Edit Horario
 		 * @since 31/10/2021
